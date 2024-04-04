54bbf3b1d5ab380ba5f7e9e33dc514be26866838
sunainatarimela <164285332+sunainatarimela@users.noreply.github.com>
2024-04-04 12:11:52 -0500

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -35,7 +35,7 @@ set_background("govcontracts.jpg")
 with open("uicbusiness.png", "rb") as f:
     data = base64.b64encode(f.read()).decode("utf-8")
 
-    st.sidebar.markdown(
+    st.markdown(
         f"""
         <div style="margin-top:-.5%;margin-left:-5%;">
             <img src="data:image/png;base64,{data}" width="200" height="100">
