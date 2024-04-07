3eae95936f130503ca29b734cc0a9ec79964f1b1
sunainatarimela <164285332+sunainatarimela@users.noreply.github.com>
2024-04-07 11:38:14 -0500

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -29,7 +29,7 @@ def set_background(png_file):
     </style>
     ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
-set_background("GovernmentContract_1.png")
+set_background("GovernmentContract_2.png")
 
 with open("uicbusiness.png", "rb") as f:
     data = base64.b64encode(f.read()).decode("utf-8")
