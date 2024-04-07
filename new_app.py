64089f1d4601027f5757c32088162a045fc6f552
sunainatarimela <164285332+sunainatarimela@users.noreply.github.com>
2024-04-06 23:34:18 -0500

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -17,7 +17,6 @@ def get_base64(bin_file):
     return base64.b64encode(data).decode()
 
 def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
     page_bg_img = '''
     <style>
     body {
@@ -25,7 +24,7 @@ def set_png_as_page_bg(png_file):
     background-size: cover;
     }
     </style>
-    ''' % bin_str
+    ''' 
     
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
