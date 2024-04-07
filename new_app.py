b831c01f03be00facfea88ce9f268f00eec9563d
sunainatarimela <164285332+sunainatarimela@users.noreply.github.com>
2024-04-07 16:48:57 -0500

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -366,8 +366,28 @@ def contract_duration_predict():
     import base64
     import streamlit as st
     import streamlit.components.v1 as components
+    from streamlit.components.v1 import html
 
-        
+
+    def get_base64(bin_file):
+        with open(bin_file, 'rb') as f:
+            data = f.read()
+        return base64.b64encode(data).decode()
+
+    def set_background(png_file):
+        bin_str = get_base64(png_file)
+        page_bg_img = '''
+        <style>
+        .stApp {
+        background-image: linear-gradient(rgba(255,255,255,0.75), rgba(255,255,255,0.75)), url("data:image/png;base64,%s");
+        background-size:cover;
+        background-repeat:no-repeat;
+        position: absolute;
+        }
+        </style>
+        ''' % bin_str
+        st.markdown(page_bg_img, unsafe_allow_html=True)
+    set_background("GovernmentContract_Dur.png")   
 
     def construct_sample_duration(input):
 
