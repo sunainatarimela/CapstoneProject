84688adc9b4914dd98a6e73c490a2c7e9482c583
sunainatarimela <164285332+sunainatarimela@users.noreply.github.com>
2024-04-07 11:25:26 -0500

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -23,8 +23,8 @@ def set_background(png_file):
     <style>
     .stApp {
     background-image: linear-gradient(rgba(255,255,255,0.85), rgba(255,255,255,0.85)), url("data:image/png;base64,%s");
-    background-size:contain;
-   background-repeat:no-repeat;
+    background-size:75% 50%;
+    background-repeat:no-repeat;
     }
     </style>
     ''' % bin_str
