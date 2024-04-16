f52048b3054c194a5014b896cdfc8c1a4321f532
sunainatarimela <164285332+sunainatarimela@users.noreply.github.com>
2024-04-16 14:51:27 -0500

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -77,9 +77,13 @@ def intro():
     )
 
     
-    st.write("# ----------------------------------------------------------------")
+    st.write("# ------------------------------------------------")
+    st.write("Project Collaborators")
     st.image("Images/l1-modified.png",width=40,height=40)
-
+    st.image("Images/l2-modified.png",width=40,height=40)
+    st.image("Images/l3-modified.png",width=40,height=40)
+    st.image("Images/l4-modified.png",width=40,height=40)
+    st.image("Images/l5-modified.png",width=40,height=40)
                                                                
 def business_type_predict():
     import pandas as pd
