7b1a12c1996ad2c59c9730f4342e165f0863e3c3
sunainatarimela <164285332+sunainatarimela@users.noreply.github.com>
2024-04-16 15:11:16 -0500

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -96,16 +96,16 @@ def intro():
     for idx, img in enumerate(linkedin_imgs): 
         cols = st.columns(5) 
         
-        cols[0].image(linkedin_imgs[idx], use_column_width=50)
+        cols[0].image(linkedin_imgs[idx], use_column_width=True)
         idx+=1
-        cols[1].image(linkedin_imgs[idx], use_column_width=50)
+        cols[1].image(linkedin_imgs[idx], use_column_width=True)
         idx+=idx
         cols[2].image(linkedin_imgs[idx], use_column_width=50)
         idx+=idx
         cols[3].image(linkedin_imgs[idx], use_column_width=50)
         idx+=idx
         cols[4].image(linkedin_imgs[idx], use_column_width=50)
-        idx+=idx
+        
 
 
 def business_type_predict():
